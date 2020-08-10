dfa845dca8fdc9fb13ff35e7f3bebcc7d8cc552e
Anthony <logicainformatica18@gmail.com>
2020-08-10 17:14:33 -0500

    habilitando blog

---
+++ blog/head.php
@@ -0,0 +1,54 @@
+    <link rel="stylesheet" href="../assets/web/assets/mobirise-icons-bold/mobirise-icons-bold.css">
+    <link rel="stylesheet" href="../assets/web/assets/mobirise-icons2/mobirise2.css">
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="../assets/dropdown/css/style.css">
+    <link rel="stylesheet" href="../assets/tether/tether.min.css">
+    <link rel="stylesheet" href="../assets/socicon/css/styles.css">
+    <link rel="stylesheet" href="../assets/theme/css/style.css">
+    <link rel="preload" as="style" href="../assets/mobirise/css/mbr-additional.css">
+    <link rel="stylesheet" href="../assets/mobirise/css/mbr-additional.css" type="text/css">
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="function.js"></script>
+</head>
+
+<body>
+
+<!-- Image and text -->
+<nav class="navbar navbar bg-black" style="background-color:black">
+  <a class="navbar-brand" href="../index.php">
+    <img src="../dist/img/logo.png" width="30" height="30" class="fa-arrow-circle-left">
+    Lógica Informática
+  </a>
+  <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+  </form>
+</nav>
+
+
+
+
+
+<style>
+.titulo_post{
+    color:crimson;
+   /* font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;*/
+    font-size:50px;
+
+}
+</style>
+
+
+
+    <div class="row">
+        <div class="col-md-8 col-lg-8 ">
+            <div class="card" >
+
+                <div class="card-body">
+                        <p></p>
+                  
+    
