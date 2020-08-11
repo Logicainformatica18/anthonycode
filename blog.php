de49f6d1a218e1fb5212abee09f43a3b168d2b0b
Anthony <logicainformatica18@gmail.com>
2020-08-11 02:15:06 -0500

    arreglando blog.php

--- blog.php
+++ blog.php
@@ -2,82 +2,95 @@
 include "functionblog.php";
 ?>
 <!DOCTYPE html>
-<html>
-
+<html  >
 <head>
-    <!-- Site made with Mobirise Website Builder v5.0.15, https://mobirise.com -->
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="generator" content="Mobirise v5.0.15, mobirise.com">
-    <meta name="viewport" content="width=device-width, initial-scale=1,
-            minimum-scale=1">
-    <link rel="shortcut icon" href="assets/images/logo4.png" type="image/x-icon">
-    <meta name="description" content="Web Maker Description">
-
-
-    <title>Educación</title>
-    <link rel="stylesheet" href="assets/web/assets/mobirise-icons2/mobirise2.css">
-    <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
-    <link rel="stylesheet" href="assets/tether/tether.min.css">
-    <link rel="stylesheet" href="assets/dropdown/css/style.css">
-    <link rel="stylesheet" href="assets/socicon/css/styles.css">
-    <link rel="stylesheet" href="assets/theme/css/style.css">
-    <link rel="stylesheet" href="assets/gallery/style.css">
-    <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css">
-    <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
-
-
-
+  <!-- Site made with Mobirise Website Builder v5.0.15, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v5.0.15, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo4.png" type="image/x-icon">
+  <meta name="description" content="Web Maker Description">
+  
+  
+  <title>Anthony Code</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons2/mobirise2.css">
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/gallery/style.css">
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  <script src="function.js"></script>
+  
 </head>
 
 <body>
-    <section class="menu cid-s60WIYg65q" once="menu" id="menu2-32">
+<section class="menu cid-s60WIYg65q" once="menu" id="menu2-32">
 
+    
 
+<nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm bg-color transparent">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <div class="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </button>
+    <div class="menu-logo">
+        <div class="navbar-brand">
+            <span class="navbar-logo">
+                <a href="index.php">
+                    <img src="assets/images/logo-logicainformatica-photoshop-121x121.png" alt="A" title="" style="height: 3.8rem;">
+                </a>
+            </span>
+            <span class="navbar-caption-wrap"><a class="navbar-caption text-success display-4" href="index.php">Anthony Code&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</a></span>
+        </div>
+    </div>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true"><li class="nav-item"><a class="nav-link link text-white display-4" href="login.php"><span class="mbri-users mbr-iconfont mbr-iconfont-btn"></span>Login</a></li><li class="nav-item"><a class="nav-link link text-white display-4" href="cursos.php"><span class="mobi-mbri mobi-mbri-contact-form mbr-iconfont mbr-iconfont-btn"></span>Cursos</a></li><li class="nav-item">
+                <a class="nav-link link text-white display-4" href="blog.php"><span class="socicon socicon-bloglovin mbr-iconfont mbr-iconfont-btn"></span>Blog</a>
+            </li></ul>
+        <div class="navbar-buttons mbr-section-btn"><a class="btn btn-sm btn-info display-4" href="#" onclick="envioWhatsapp('anthonycode')"><span class="socicon socicon-whatsapp mbr-iconfont mbr-iconfont-btn"></span>
+                
+                +51-997852483</a></div>
+    </div>
+</nav>
+</section>
+<section class="mbr-section info3 cid-s7ksMnJa7N" id="info3-3g">
 
-        <nav class="navbar navbar-expand beta-menu navbar-dropdown
-                align-items-center navbar-fixed-top navbar-toggleable-sm">
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <div class="hamburger">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
-            </button>
-            <div class="menu-logo">
-                <div class="navbar-brand">
-                    <span class="navbar-logo">
-                        <a href="index.php">
-                            <img src="assets/images/logica.png" alt="Lógica" style="height: 3.8rem;">
-                        </a>
-                    </span>
-                    <span class="text-light">Lógica Informática</span>
-                </div>
+  
+
+    <div class="mbr-overlay" style="opacity: 0.3; background-color: rgb(0, 0, 0);">
+    </div>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="media-container-column title col-12 col-md-10">
+                <!-- <h2 class="align-left mbr-bold mbr-white pb-3 mbr-fonts-style display-2">
+                    BACKGROUND IMAGE
+                </h2> -->
+                <!-- <h3 class="mbr-section-subtitle align-left mbr-light mbr-white pb-3 mbr-fonts-style display-5">
+                    Shape your future web project with sharp design and refine coded functions
+                </h3>
+                <p class="mbr-text align-left mbr-white mbr-fonts-style display-7">
+                    Background image with solid color overlay and parallax effect. Develop fully responsive, mobile-ready websites that look amazing on any devices and browsers
+                </p> -->
+                <!-- <div class="mbr-section-btn align-left py-4">
+                    <a class="btn btn-primary display-4" href="https://mobiri.se">LEARN MORE</a>
+                    <a class="btn btn-white-outline display-4" href="https://mobiri.se">LIVE DEMO</a>
+                </div> -->
             </div>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true">
-                    <li class="nav-item"><a class="nav-link link text-white
-                                display-4" href="login.php"><span class="mbri-users mbr-iconfont
-                                    mbr-iconfont-btn"></span>Iniciar Sesión</a></li>
-                    <li class="nav-item"><a class="nav-link link text-white
-                                display-4" href="login.php"><span class="mobi-mbri mobi-mbri-contact-form
-                                    mbr-iconfont mbr-iconfont-btn"></span>Cursos</a></li>
-                    <li class="nav-item">
-                        <a class="nav-link link text-white display-4" href="#"><span class="socicon socicon-bloglovin
-                                    mbr-iconfont mbr-iconfont-btn"></span>Blog</a>
-                    </li>
-                </ul>
-
-            </div>
-        </nav>
-    </section>
-
-
-
+        </div>
+    </div>
+</section>
 
 
 
@@ -113,124 +126,79 @@ include "functionblog.php";
 
 
    
+    <pre>
 
 
+</pre>
 
-    <section class="footer4 cid-s60ZyBNVXx" id="footer4-36">
+    <section once="footers" class="cid-s7kpmGA6pD" id="footer7-3f">
 
+    
 
+    
 
-
-
-        <div class="container">
-            <div class="media-container-row content mbr-white">
-                <div class="col-md-3 col-sm-4">
-
-                    <div class="fb-page" data-href="https://www.facebook.com/Logicainformatica2020/" data-tabs="timeline" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
-                        <blockquote cite="https://www.facebook.com/Logicainformatica2020/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Logicainformatica2020/">Lógica
-                                Informática</a></blockquote>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-8">
-                    <p class="mb-4 foot-title mbr-fonts-style
-                                display-7">
-                        Dedicatoria
-                    </p>
-                    <p class="mbr-text mbr-fonts-style foot-text
-                                display-7">
-                        Dedico este Proyecto informático en primer lugar
-                        a dios, a mi madre, a mis alumnos, a mis
-                        compañeros con quienes compartimos bellos
-                        momentos, algunos tristes, otros alegres en
-                        donde fuimos de menos a más conociendo todo el
-                        mundo de la tecnología.
-                    </p>
-                </div>
-                <div class="col-md-4 offset-md-1 col-sm-12">
-
-                    <p class="mb-4 mbr-fonts-style foot-title
-                                display-7">
-                        Contacto</p>
-                    <div class="social-list pl-0 mb-0">
-                        <div class="soc-item">
-                            <a href="https://twitter.com/mobirise" target="_blank">
-                                <span class="socicon-twitter socicon
-                                            mbr-iconfont mbr-iconfont-social"></span>
-                            </a>
-                        </div>
-                        <div class="soc-item">
-                            <a href="https://www.facebook.com/pages/Mobirise/1616226671953247" target="_blank">
-                                <span class="socicon-facebook socicon
-                                            mbr-iconfont mbr-iconfont-social"></span>
-                            </a>
-                        </div>
-                        <div class="soc-item">
-                            <a href="https://www.youtube.com/c/mobirise" target="_blank">
-                                <span class="socicon-youtube socicon
-                                            mbr-iconfont mbr-iconfont-social"></span>
-                            </a>
-                        </div>
-                        <div class="soc-item">
-                            <a href="https://instagram.com/mobirise" target="_blank">
-                                <span class="socicon-instagram socicon
-                                            mbr-iconfont mbr-iconfont-social"></span>
-                            </a>
-                        </div>
-                        <div class="soc-item">
-                            <a href="https://plus.google.com/u/0/+Mobirise" target="_blank">
-                                <span class="socicon-googleplus socicon
-                                            mbr-iconfont mbr-iconfont-social"></span>
-                            </a>
-                        </div>
-                        <div class="soc-item">
-                            <a href="https://www.behance.net/Mobirise" target="_blank">
-                                <span class="socicon-behance socicon
-                                            mbr-iconfont mbr-iconfont-social"></span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="footer-lower">
-                <div class="media-container-row">
-                    <div class="col-sm-12">
-                        <hr>
-                    </div>
-                </div>
-                <div class="media-container-row mbr-white">
-                    <div class="col-sm-12 copyright">
-                        <p class="mbr-text mbr-fonts-style display-7">
-                            © Copyright 2020&nbsp; - All Rights Reserved
-                        </p>
-                    </div>
+<div class="container">
+    <div class="media-container-row align-center mbr-white">
+        <div class="row row-links">
+            <ul class="foot-menu">
+                
+                
+                
+                
+                
+            <li class="foot-menu-item mbr-fonts-style display-7">
+                    <a class="text-white mbr-bold" href="#" target="_blank">Contáctame</a>
+                </li>
+            </ul>
+        </div>
+        <div class="row social-row">
+            <div class="social-list align-right pb-2">
+                
+                
+                
+                
+                
+                
+            <div class="soc-item">
+                    <a href="https://www.facebook.com/Logicainformatica2020/" target="_blank">
+                        <span class="socicon-facebook socicon mbr-iconfont mbr-iconfont-social"></span>
+                    </a>
+                </div><div class="soc-item">
+                    <a href="https://www.youtube.com/c/anthonycode" target="_blank">
+                        <span class="socicon-youtube socicon mbr-iconfont mbr-iconfont-social"></span>
+                    </a>
                 </div>
             </div>
         </div>
-    </section>
+        <div class="row row-copirayt">
+            <p class="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-7">
+                © Copyright 2020 - All Rights Reserved
+            </p>
+ 
+        </div>
+    </div>
+</div>
+</section>
 
 
-    <script src="assets/web/assets/jquery/jquery.min.js"></script>
-    <script src="assets/popper/popper.min.js"></script>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-    <script src="assets/tether/tether.min.js"></script>
-    <script src="assets/smoothscroll/smooth-scroll.js"></script>
-    <script src="assets/dropdown/js/nav-dropdown.js"></script>
-    <script src="assets/dropdown/js/navbar-dropdown.js"></script>
-    <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
-    <script src="assets/ytplayer/jquery.mb.ytplayer.min.js"></script>
-    <script src="assets/vimeoplayer/jquery.mb.vimeo_player.js"></script>
-    <script src="assets/bootstrapcarouselswipe/bootstrap-carousel-swipe.js"></script>
-    <script src="assets/mbr-testimonials-slider/mbr-testimonials-slider.js"></script>
-    <script src="assets/masonry/masonry.pkgd.min.js"></script>
-    <script src="assets/imagesloaded/imagesloaded.pkgd.min.js"></script>
-    <script src="assets/theme/js/script.js"></script>
-    <script src="assets/slidervideo/script.js"></script>
-    <script src="assets/gallery/player.min.js"></script>
-    <script src="assets/gallery/script.js"></script>
-    <script src="assets/formoid/formoid.min.js"></script>
+<script src="assets/web/assets/jquery/jquery.min.js"></script>
+<script src="assets/popper/popper.min.js"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/tether/tether.min.js"></script>
+<script src="assets/smoothscroll/smooth-scroll.js"></script>
+<script src="assets/dropdown/js/nav-dropdown.js"></script>
+<script src="assets/dropdown/js/navbar-dropdown.js"></script>
+<script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+<script src="assets/bootstrapcarouselswipe/bootstrap-carousel-swipe.js"></script>
+<script src="assets/mbr-testimonials-slider/mbr-testimonials-slider.js"></script>
+<script src="assets/masonry/masonry.pkgd.min.js"></script>
+<script src="assets/imagesloaded/imagesloaded.pkgd.min.js"></script>
+<script src="assets/vimeoplayer/jquery.mb.vimeo_player.js"></script>
+<script src="assets/theme/js/script.js"></script>
+<script src="assets/gallery/player.min.js"></script>
+<script src="assets/gallery/script.js"></script>
+<script src="assets/slidervideo/script.js"></script>
 
-    <div id="fb-root"></div>
-    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v7.0" nonce="dxXtDy2M"></script>
+
 </body>
-
 </html>
